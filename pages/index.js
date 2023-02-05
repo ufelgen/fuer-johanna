23275f31b63d8f6983f4e3a5902baf81d9dbd693
ufelgen <felgenhauer.u@gmail.com>
2023-02-05 16:11:53 +0100

    try confetti

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import Footer from "../components/Footer";
 import { getGreeting } from "../helpers/getGreeting";
 import styled from "styled-components";
 import { useState } from "react";
+import format from "date-fns/format";
+import Confetti from "react-confetti";
+import useWindowDimensions from "../helpers/useWindowSize";
 
 export default function Home() {
   const [image, setImage] = useState(false);
@@ -14,8 +17,13 @@ export default function Home() {
 
   const greeting = getGreeting();
 
+  const today = format(new Date(), "dd-MM");
+
+  const { height, width } = useWindowDimensions();
+
   return (
     <StyledMain>
+      {today === "05-02" && <Confetti width={width} height={height} />}
       {image ? (
         <RandomImage randomImage={handleRandomImage} />
       ) : (
