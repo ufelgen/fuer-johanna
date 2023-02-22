6df85cba30a9ee46a07921a29519fb26985fb7b3
ufelgen <felgenhauer.u@gmail.com>
2023-02-22 17:48:28 +0100

    clicking advice button several times does always return same item. unsuccessful in finding out why

--- pages/advice/index.js
+++ pages/advice/index.js
@@ -6,16 +6,21 @@ import RandomAdvice from "../../components/RandomAdvice";
 
 export default function Advice() {
   const [advice, setAdvice] = useState("");
+  const url = "https://api.adviceslip.com/advice";
 
   async function fetchAdvice() {
     try {
-      const response = await fetch("https://api.adviceslip.com/advice");
+      const response = await fetch(url);
       const data = await response.json();
       setAdvice(data.slip);
     } catch (error) {
       console.error(error);
     }
   }
+
+  setTimeout(() => {
+    setAdvice("");
+  }, "10000");
 
   return (
     <StyledAdvicePage>
