5de67820b98201590bd31d9bf93c296f6b4df022
ufelgen <felgenhauer.u@gmail.com>
2025-03-18 15:02:29 +0100

    change photos to random from collection

--- components/RandomImageFetch.js
+++ components/RandomImageFetch.js
@@ -1,9 +1,25 @@
-import { useFetch } from "../helpers/useFetch";
+import { useState, useEffect } from "react";
 
-export default function RandomImageFetch() {
-  const image = useFetch(
-    "https://api.unsplash.com/photos/random/?client_id=DO-2ZSrlvG9NonSxNNbh2szxV1gXNj3tQqPiyDg8y_A"
-  );
+export default function RandomImageFetch(collectionId) {
+  const [data, setData] = useState([]);
+  const accessKey = "DO-2ZSrlvG9NonSxNNbh2szxV1gXNj3tQqPiyDg8y_A";
 
-  return image;
+  useEffect(() => {
+    async function startFetching() {
+      try {
+        const response = await fetch(
+          `https://api.unsplash.com/collections/${collectionId}/photos?client_id=${accessKey}&per_page=10`
+        );
+        const data = await response.json();
+        if (data.length > 0) {
+          const randomImage = data[Math.floor(Math.random() * data.length)];
+          setData(randomImage.urls.regular);
+        }
+      } catch (error) {
+        console.error("du kannst gar nichts");
+      }
+    }
+    startFetching();
+  }, [collectionId]);
+  return data;
 }
